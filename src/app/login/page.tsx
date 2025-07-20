3feb9a332c870415138f62beadb342092bdfe020
Jasmit Bajariya <jasmitbajaria25@gmail.com>
2025-07-20 19:09:15 +0530

    sign and login page working

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,20 +1,44 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 export default function loginPage() {
+  const router = useRouter();
   const [user, setUser] = React.useState({
     email: "",
     password: "",
   });
 
-  const onlogin = async () => {};
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+
+  const onlogin = async (event: React.MouseEvent) => {
+    event.preventDefault();
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/login", user);
+      console.log("Login success", response.data);
+      router.push("/profile");
+    } catch (error: any) {
+      console.log("Login failed", error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (user.email.length > 0 && user.password.length > 0) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
 
   return (
     <div className="flex h-screen items-center justify-center bg-gray-200">
       <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-lg">
-        <h2 className="text-2xl font-bold text-center">login</h2>
+        <h2 className="text-2xl font-bold text-center">
+          {loading ? "Loading..." : "Login"}
+        </h2>
         <form className="space-y-4">
           <div>
             <label
@@ -55,7 +79,7 @@ export default function loginPage() {
             onClick={onlogin}
             className="w-full py-2 px-4  bg-linear-to-r from-gray-500 to-gray-800 text-white font-semibold rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2 cursor-pointer"
           >
-            login
+            {buttonDisabled ? "no login" : "login"}
           </button>
         </form>
         <Link
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 export default function SignupPage() {
@@ -11,12 +11,42 @@ export default function SignupPage() {
     username: "",
   });
 
-  const onSignup = async () => {};
+  const [buttonDisabled, setButtonDisabled] = React.useState(false);
+
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.username.length > 0
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
+
+  const [loading, setLoading] = React.useState(false);
+
+  const onSignup = async (event: React.MouseEvent) => {
+    event.preventDefault();
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/signup", user);
+      console.log("Signup success", response.data);
+      router.push("/login");
+    } catch (error: any) {
+      console.log("Signup failed", error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <div className="flex h-screen items-center justify-center bg-gray-200">
       <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-lg">
-        <h2 className="text-2xl font-bold text-center">Signup</h2>
+        <h2 className="text-2xl font-bold text-center">
+          {loading ? "Processing" : "Sign Up"}
+        </h2>
         <form className="space-y-4">
           <div>
             <label
@@ -74,7 +104,7 @@ export default function SignupPage() {
             onClick={onSignup}
             className="w-full py-2 px-4  bg-linear-to-r from-gray-500 to-gray-800 text-white font-semibold rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2 cursor-pointer"
           >
-            Signup
+            {buttonDisabled ? "No signup" : "Signup"}
           </button>
         </form>
         <Link
