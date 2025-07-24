26cd9ad23459a6023966988309cc417715c335c9
Jasmit Bajariya <jasmitbajaria25@gmail.com>
2025-07-24 17:10:47 +0530

    verfying email feature added

---
+++ src/app/verifyemail/page.tsx
@@ -0,0 +1,52 @@
+"use client";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+
+export default function VerifyEmailPage() {
+  const router = useRouter();
+  const [token, setToken] = useState(" ");
+  const [verfied, setVerified] = useState(false);
+  const [error, setError] = useState(false);
+
+  const verifyUserEmail = async () => {
+    try {
+      await axios.post("/api/users/verifyemail", { token });
+      setVerified(true);
+    } catch (error: any) {
+      setError(true);
+      console.log(error.response.data);
+    }
+  };
+
+  useEffect(() => {
+    if (token.length > 0) verifyUserEmail();
+  }, [token]);
+
+  useEffect(() => {
+    const urlToken = window.location.search.split("=")[1];
+    setToken(urlToken || "");
+  }, []);
+
+  return (
+    <div className="flex flex-col justify-center min-h-screen py-2">
+      <h1 className="text-4xl font-bold text-center">Verify Email</h1>
+      <h2 className="p-2 bg-orange-500 text-black">
+        {token ? `${token}` : "no token"}
+      </h2>
+
+      {verfied && (
+        <div>
+          <h2 className="text-2xl">Email Verified</h2>
+          <Link href="/login">Login</Link>
+        </div>
+      )}
+      {error && (
+        <div>
+          <h2 className="text-2xl text-red-500">Error</h2>
+        </div>
+      )}
+    </div>
+  );
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -4,17 +4,19 @@ import type { NextRequest } from "next/server";
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
-  const publicPaths = path == "/login" || path == "/signup";
+  const publicPaths =
+    path == "/login" || path == "/signup" || path == "/verifyemail";
 
   const token = request.cookies.get("token")?.value || "";
   if (publicPaths && token) {
     return NextResponse.redirect(new URL("/", request.nextUrl));
-  } else if (!publicPaths && !token) {
+  }
+  if (!publicPaths && !token) {
     return NextResponse.redirect(new URL("/login", request.nextUrl));
   }
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/", "/profile", "/signup", "/login"],
+  matcher: ["/", "/profile", "/signup", "/login", "/verifyemail"],
 };
